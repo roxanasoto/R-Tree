28d01f770243ec8e481e3e67b7a9473833af95b0
LizLima <lizbethlima@gmail.com>
2018-08-03 12:33:06 -0500

    Cambio clase node

--- Rtree_core/Node.h
+++ Rtree_core/Node.h
@@ -8,15 +8,27 @@ class Node
     public:
     //Informacion de que area abarca cada nodo
     bool isLeaf;
-    bool isInterno;
 
-    //Nodos internos
-    vector<Region> rchildren;
-    int r_children;
+    //Puntero a array de key-regiones / nodo interno
+   // Region* keyRegion;
+   vector<Region*> keyRegion;
+   // Node* children;
+   vector<Node*> children;
+    int countKey;
 
-    //Nodos hojas contienen elementos
-    vector<Element> echildren;
-    int e_children;
+    //Puntero a array de elementos / nodo hoja
+    Node* parent;
+    vector<Element*> echildren;// Element* echildren;
+    int countElement;
 
+    //cada entrada  contiene el (mbr, oid)
+    //mbr: espacio que contiene el objeto
+    //oid identificador del objeto
+    //element tambien deberia tener una region 
+    //y un identificador
+
+    //Minimo de entradas en el root es 2
+    //Si root es hoja  entonces puede ser 0 o 1
+    //Todas las hojas estan al mismo nivel
     
 };
