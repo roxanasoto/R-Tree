f4f4adafcd4e2a3d77de9345feb5859b721386c0
Unknown <roxana.soto.barrera@gmail.com>
2018-09-03 09:36:59 -0500

    roxana Soto cambio en element.cpp

--- Rtree_core/Element.cpp
+++ Rtree_core/Element.cpp
@@ -41,7 +41,7 @@ Element::Element(Polygon pol)
 
 void Element::setOid(int id)
 {
-	oid = id;
+  oid = id;
 }
 
 int Element::getOid()
@@ -61,46 +61,35 @@ Polygon Element::getPolygon()
 
 float Element::getAreaPol()
 {
-	//if (mbr != NULL)
-	//{
-		float axisX = mbr.coordX - mbr.coordMinX;
-		float axisY = mbr.coordY - mbr.coordMinY;
-		return axisX*axisY;
-	//}
+  //if (mbr != NULL)
+  //{
+    float axisX = mbr.coordX - mbr.coordMinX;
+    float axisY = mbr.coordY - mbr.coordMinY;
+    return axisX*axisY;
+  //}
 }
 
 void Element::print()
 {
-	for (int i = 0; i < poligono.getPoints().size(); i++)
-	{
-		cout << "(" << poligono.getPoints().at(i).getX() << "," << poligono.getPoints().at(i).getY() << ") " ;
-	}
-	cout << endl;
+  for (int i = 0; i < poligono.getPoints().size(); i++)
+  {
+    cout << "(" << poligono.getPoints().at(i).getX() << "," << poligono.getPoints().at(i).getY() << ") " ;
+  }
+  cout << endl;
 }
 
 
 float Element::distanceToPoint(Point p){
   Point difference;
   int distanceResult;
-  if (p.getX() > mbr.coordMinX)
-  {
-    if (p.getX() < mbr.coordX)
-        difference.setX(0);
-    else
-      difference.setX(p.getX() - mbr.coordX);
-  }
-  else
-  difference.setX(mbr.coordMinX - p.getX());
-  if (p.getY() > mbr.coordMinY)
-  {
-    if (p.getY() < mbr.coordY)
-      difference.setY(0);
-    else
-      difference.setY( p.getY() - mbr.coordY);
-  }
-  else
-    difference.setY(mbr.coordMinY - p.getY());
 
-  this->distance = sqrt(difference.getX()*difference.getX() + difference.getY()*difference.getY());
+  vector<Point> poli=poligono.getPoints();
+  distance=sqrt((p.getX()-poli[0].getX())*(p.getX()-poli[0].getX())+(p.getY()-poli[0].getY())*(p.getY()-poli[0].getY()));
+  for(auto punto:poli)
+  {
+    float temp=sqrt((p.getX()-punto.getX())*(p.getX()-punto.getX())+(p.getY()-punto.getY())*(p.getY()-punto.getY()));
+    if(temp<distance)
+      distance=temp;
+  }
   return this->distance;
 }
